38373a373e9afbac8a24a3c3053dda1129e15c2f
Yi Qiao <atrusqiao@gmail.com>
2018-12-18 10:54:35 -0700

    updated data volume version

--- fetch-data.sh
+++ fetch-data.sh
@@ -3,7 +3,7 @@
 fetch() {
     if [ ! -e data-vol/.${1}.fetched ]; then
         echo "Fetching ${1}"
-        curl -L\# https://s3.amazonaws.com/iobio/assets/data-vol/v2.0/${1}.tar.gz | tar -C data-vol -xzf -
+        curl -L\# https://s3.amazonaws.com/iobio/assets/data-vol/v2.4.1/${1}.tar.gz | tar -C data-vol -xzf -
         if [ $? -eq 0 ]; then touch data-vol/.${1}.fetched; fi
     fi
 }
@@ -16,5 +16,3 @@ fetch hpo
 fetch ref
 fetch vep-cache
 fetch local_cache
-
-
